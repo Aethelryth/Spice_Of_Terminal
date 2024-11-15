e678ad7b212e9bbd7601b81e95c79ffbc635889a
Aethelryth <piggytron2000@gmail.com>
2024-11-15 17:22:29 +0800

    Updated final instance colo(u)r to have ambiguous (u)

--- Example.py
+++ Example.py
@@ -1,4 +1,4 @@
 from Spice_Of_Terminal import FG, BG
 
-print(f"{FG.Green}3-Bit Colo(u)r is great, {FG.BrightGreen}and so is 4-bit.{FG.Clear}")
-print(f"{FG.rgb(0,200,100)}But I prefer the full colour spectrum{FG.Clear}")
+print(f"{FG.Green}3-Bit colo(u)r is great, {FG.BrightGreen}and so is 4-bit.{FG.Clear}")
+print(f"{FG.rgb(0,200,100)}But I prefer the full colo(u)r spectrum{FG.Clear}")
